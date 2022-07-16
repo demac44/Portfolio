f64c9e1468b92bc1e8162b278c36f461dbd8a10d
demac44 <demirumejr2@gmail.com>
2022-07-16 20:37:32 +0300

    New design

--- script.js
+++ script.js
@@ -1,17 +1,73 @@
-const arrow = document.querySelector(".logo-drop2")
-const arrow2 = document.querySelector(".logo-drop3")
-const proj_titles = document.querySelectorAll(".proj-title")
+const particles_container = document.querySelector(".particles-container")
+const particles = document.querySelectorAll(".particle")
+
+const WIDTH = window.innerWidth - 20
+const HEIGHT = window.innerHeight -20
+
+for(let i = 0;i<200;i++){
+    const newParticle = document.createElement("div")
+
+    newParticle.classList.add("particle")
+    let pos_x = Math.floor(Math.random() * (WIDTH))
+    let pos_y = Math.floor(Math.random() * (HEIGHT))
+    newParticle.style.top = `${pos_y}px`
+    newParticle.style.left = `${pos_x}px`
+
+    newParticle.style.width = `${Math.floor(Math.random() * (5 - 1 + 1) + 1)}px`
+    newParticle.style.height = `${Math.floor(Math.random() * (5 - 1 + 1) + 1)}px`
+
+
+    // ANIMATE PARTICLES
+    let moving
+
+    const direction = Math.floor(Math.random() * (4 - 1 + 1) + 1)
+
+    if(direction === 1){
+        moving = [
+            {top: `${pos_y}px`, left: `${pos_x}px`},
+            {left: `${pos_x}px`, top: `${pos_y + 100}px`},
+            {left: `${pos_x + 100}px`, top: `${pos_y + 100}px`},
+            {left: `${pos_x + 100}px`, top: `${pos_y}px`},
+            {left: `${pos_x}px`, top: `${pos_y}px`},
+        ]
+    } else if(direction === 2){
+        moving = [
+            {top: `${pos_y}px`, left: `${pos_x}px`},
+            {left: `${pos_x}px`, top: `${pos_y - 100}px`},
+            {left: `${pos_x + 100}px`, top: `${pos_y - 100}px`},
+            {left: `${pos_x + 100}px`, top: `${pos_y + 100}px`},
+            {left: `${pos_x}px`, top: `${pos_y}px`},
+        ]
+    } else if(direction === 3){
+        moving = [
+            {top: `${pos_y}px`, left: `${pos_x}px`},
+            {left: `${pos_x - 100}px`, top: `${pos_y}px`},
+            {left: `${pos_x - 100}px`, top: `${pos_y + 100}px`},
+            {left: `${pos_x + 100}px`, top: `${pos_y + 100}px`},
+            {left: `${pos_x}px`, top: `${pos_y}px`},
+        ]
+    } else if(direction === 4){
+        moving = [
+            {top: `${pos_y}px`, left: `${pos_x}px`},
+            {left: `${pos_x + 100}px`, top: `${pos_y}px`},
+            {left: `${pos_x + 100}px`, top: `${pos_y + 100}px`},
+            {left: `${pos_x - 100}px`, top: `${pos_y}px`},
+            {left: `${pos_x}px`, top: `${pos_y}px`},
+        ]
+    }
+    const timing = {
+        duration: Math.floor(Math.random() * (20000 - 15000 + 1) + 15000),
+        iterations: Infinity
+    }
+
+    newParticle.animate(moving, timing)
+
+
+    // ADD PARTICLE
+    particles_container.append(newParticle)
+
+}
+
 
 document.querySelector('.contact-btn').addEventListener('click', ()=>window.scroll({left: 0, top: document.body.scrollHeight, behavior: "smooth"}))
 
-// proj_titles.forEach(title => {
-//     title.style.border = "2px solid " + "#" + ((1<<24)*Math.random() | 0).toString(16)
-// })
-
-setTimeout(() => {
-    arrow.classList.remove("logo-drop2")
-    arrow.classList.add("logo-drop3")
-    arrow.addEventListener("click", () => {
-        window.scroll({left:0, top: window.innerHeight, behavior: "smooth"})
-    })
-}, 2400)
